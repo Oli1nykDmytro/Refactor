6635ba47e4b148bd1662ebd7843a3c991ffc5844
OleynikDima <57680550+OleynikDima@users.noreply.github.com>
2021-02-15 10:42:35 +0200

    add type-detail

---
+++ src/typeDefs/type-details.ts
@@ -0,0 +1,16 @@
+type EventDetailsProps = {
+    event?: Event;
+    open: boolean;
+    setOpen: (open: boolean) => void;
+    onDialogClose?: () => void;
+    refetchEvents?: () => void;
+    message?: Maybe<Message>;
+    onEventCreation?: (eventId: string, event: Event) => void;
+    onCreateEventFromMessageItem?: (eventId: string, event: Event) => void;
+    currentUser: User;
+    onEventDelition?: () => void;
+    messageId?: string;
+    messageTitle?: string | null | undefined;
+    isMessageDone?: boolean | null | undefined;
+    isMessageDeleted?: boolean | null | undefined;
+  };
